f0d4baa6ae31ba9d3765adf1f4d6183dd914dfe8
maths <130728002+aslskks@users.noreply.github.com>
2026-03-08 11:21:36 -0600

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://aslskks.github.io/piedra-papel-o-tijeras/](https://aslskks.github.io/piedra-papel-o-tijeras/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/aslskks/piedra-papel-o-tijeras** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/aslskks/piedra-papel-o-tijeras** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,85 @@
+function check (num: number, num2: number) {
+    if (num == num2) {
+        basic.showString("Draw")
+    } else if (num == 0 && num2 == 2) {
+        basic.showIcon(IconNames.Yes)
+    } else if (num == 1 && num2 == 0) {
+        basic.showIcon(IconNames.Yes)
+    } else if (num == 2 && num2 == 1) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(500)
+    basic.clearScreen()
+    enemy_d = 0
+    enemy = 0
+    current = 0
+    dicided = 0
+    piedra()
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    enemy = receivedNumber
+    enemy_d = 1
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    control.reset()
+})
+input.onButtonPressed(Button.A, function () {
+    if (dicided == 0) {
+        if (current == 0) {
+            current = 2
+            basic.showIcon(IconNames.Scissors)
+        } else if (current == 1) {
+            current = 0
+            piedra()
+        } else if (current == 2) {
+            current = 1
+            basic.showIcon(IconNames.Square)
+        }
+    }
+})
+function piedra () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+}
+input.onButtonPressed(Button.AB, function () {
+    if (dicided == 0) {
+        radio.sendNumber(current)
+        dicided = 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (dicided == 0) {
+        if (current == 0) {
+            current = 1
+            basic.showIcon(IconNames.Square)
+        } else if (current == 1) {
+            current = 2
+            basic.showIcon(IconNames.Scissors)
+        } else if (current == 2) {
+            current = 0
+            piedra()
+        }
+    }
+})
+let enemy = 0
+let enemy_d = 0
+let dicided = 0
+let current = 0
+radio.setGroup(1)
+current = 0
+dicided = 0
+enemy_d = 0
+enemy = 0
+piedra()
+basic.forever(function () {
+    if (dicided == 1 && enemy_d == 1) {
+        check(current, enemy)
+    }
+})
